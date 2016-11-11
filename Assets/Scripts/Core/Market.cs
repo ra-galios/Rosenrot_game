273f2e1de8bbc93517ac00a711012c2aefba7330
UnderLi <maxbest007@mail.ru>
2016-11-11 22:41:17 +0300

    маркет
    новый класс

---
+++ Assets/Scripts/Core/Market.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Market : CreateSingletonGameObject<Market>
+{
+    static DateManager dateManager;
+
+    // Use this for initialization
+    void Start () {
+        var temp = dateManager.HowTimePassed("01.11.2016", DateManager.DateType.day);
+        print(temp);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
