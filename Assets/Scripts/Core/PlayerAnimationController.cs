e8befcf52928b62c0e75b1dd7bf35d6c3d522c02
PechenVladislav <pechenvladislav@gmail.com>
2017-01-04 18:29:20 +0200

    Animation fixes

---
+++ Assets/Scripts/Core/PlayerAnimationController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimationController : MonoBehaviour {
+
+    private Animator playerAnimator;
+
+    // Use this for initialization
+    void Start()
+    {
+        playerAnimator = GetComponent<Animator>();
+    } 
+
+	public void SetJump(GameInput.PlayerAction action)
+    {
+        if(action == GameInput.PlayerAction.climb)
+        {
+            playerAnimator.SetTrigger("Jump");
+        }
+        else
+        {
+            playerAnimator.SetTrigger("DoubleJump");
+        }
+    }
+
+    public void SetDoubleJump()
+    {
+        playerAnimator.SetTrigger("DoubleJump");
+    }
+
+    public void SetFall(bool isFall)
+    {
+        playerAnimator.SetBool("IsFall", isFall);
+    }
+}
