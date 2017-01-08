e826c333db5d4e221f87613f37189700ab0827e0
Halina <ra-galios@yandex.ru>
2017-01-08 05:43:27 +0300

    sounds, animations
    залита предвариетльная версия звуков
    добавлена музыка в главном меню,
    звуки кнопок,
    рабочая кнопка выключения-включения звука (но, парвда, наеврное только в
    сцене главного меню, надо модифицировать, чтобы действовало на все
    сцены)
    добавлена анимация выращивания семечка в зацепку
    добавлена анимация ЗАЦЕПА для якоба

---
+++ Assets/Scripts/UI/MuteSound.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MuteSound : MonoBehaviour
+{
+    
+     
+     public enum Trigger
+    {
+        OnClick
+    }
+
+    public Trigger trigger = Trigger.OnClick;
+    private bool muted = false;
+
+    public void OnClick()
+    {
+        if (enabled && trigger == Trigger.OnClick)
+        {
+            if (!muted)
+            {
+                AudioListener.volume = 0.0f;
+                muted = true;
+            }
+            else
+            {
+                AudioListener.volume = 1.0f;
+                muted = false;
+            }
+        }
+    }
+}
