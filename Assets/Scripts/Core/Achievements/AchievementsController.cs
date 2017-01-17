6b079b94323d5f89e1f2f1c95a4af9bb5f708daf
Kirill <kirillgrushevich@gmail.com>
2017-01-17 19:13:03 +0300

    контроллер ачивок

--- Assets/Scripts/Core/Achievements/AchievementsController.cs
+++ Assets/Scripts/Core/Achievements/AchievementsController.cs
@@ -2,46 +2,75 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+//хранение ачивок
+//если 0 - не бралась ачика
+//больше 0 - значение ачивки
+//меньше 0 - ачивка получена
+
 public static class AchievementsController 
 {
 	public enum Type 
 	{
+		//ачивки без уровня
 		GoodStart = 0,
 		RockyRoad = 1,
 		DoingGreyt = 2,
 		LetItBee = 3,
 		AbyssGazesIntoYou = 4,
-		AlwaysRight = 5,
-		ExtremeLeft = 6,
-		SelfDestructive = 7,
-		HeartBreaker = 8,
-		SurvivorFinished = 9,
-		EverybodysBestFriend = 10,
-		RubyRubyRubyRuby = 11,
-		JacobAndTheBeanstalk = 12,
-		ExplosiveBehavior = 13,
-		Catchy = 14,
-		BigBang = 15,
-		NeverGiveUp = 16,
-		GotHigh = 17,
-		Adept = 18,
-		BeOurGuest = 19,
+		BeOurGuest = 5,
+		//ачивки по уровням - на каждую заводится по 3 номера
+		AlwaysRight = 6,
+		ExtremeLeft = 9,
+		SelfDestructive = 12,
+		HeartBreaker = 15,
+		SurvivorFinished = 18,
+		EverybodysBestFriend = 21,
+		RubyRubyRubyRuby = 24,
+		JacobAndTheBeanstalk = 27,
+		ExplosiveBehavior = 30,
+		Catchy = 33,
+		BigBang = 36,
+		NeverGiveUp = 39,
+		GotHigh = 42,
+		Adept = 45,
+		
 
 	}
 
-	public static void AddToAchievement(Type type, int value)
+	public static void AddToAchievement(Type type, int addValue)
 	{
+		int val = GetAchievement(type);
+		DataManager.Instance.SetAchievement((int)type, val + addValue);
+	}
 
+	public static void CompleteAchievement(Type type)
+	{
+		DataManager.Instance.SetAchievement((int)type, -1);
 	}
 
 	public static int GetAchievement(Type type)
 	{
-		return 0;
+		return DataManager.Instance.GetAchievement((int)type);
 	}
 
-	public static bool CheckAchievement(Type type)
+	public static bool CheckAchievement(Type type, int currentValue)
 	{
+		//возвращает true только если ачивка была взята в первый раз
+		int val = GetAchievement(type);
+		if(val > 0 && currentValue >= val)
+		{
+			CompleteAchievement(type);
+			return true;
+		}
+		return false;
+	}
 
+	public static bool CheckAchievementComplete(Type type)
+	{
+		if(GetAchievement(type) < 0)
+		{
+			return true;
+		}
 		return false;
 	}
 }
