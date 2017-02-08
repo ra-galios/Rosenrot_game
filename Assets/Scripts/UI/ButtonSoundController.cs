9676950ea04cce5050ba500caf70cd7ca259d5f2
Vlad Pechen <pechenvladislav@gmail.com>
2017-02-08 15:32:01 +0300

    sounds on buttons fixes

---
+++ Assets/Scripts/UI/ButtonSoundController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonSoundController : MonoBehaviour
+{
+    public AudioSource m_AudioSource;
+
+    public AudioClip m_AcceptClip;
+    public AudioClip m_DenyClip;
+
+    // Use this for initialization
+    {
+        if (thisButton.interactable)
+        {
+            m_AudioSource.clip = m_AcceptClip;
+            m_AudioSource.Play();
+        }
+        else
+        {
+            m_AudioSource.clip = m_DenyClip;
+            m_AudioSource.Play();
+        }
+    }
+
+}
