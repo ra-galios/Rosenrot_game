4a190ca9ddb8362e1c9eb4cfd7adc447f3279a2e
sonya-s-code <ksy.sonya.kot@gmail.com>
2017-07-14 21:59:42 +0300

    Fix Ads Panel

--- Assets/Scripts/UI/PanelAds.cs
+++ Assets/Scripts/UI/PanelAds.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using UnityEngine.UI;
 using UnityEngine;
 using UnityEngine.Advertisements;
@@ -7,16 +8,41 @@ public class PanelAds : AchievementUI_Base
 {
     private AchievementUI_Leveled LevelAchievementPanel;
     private Coroutine ads;
+    private bool needShowAds;
+    private bool needRestart;
+    private DateTime needRestartTime;
 
     private void Start()
     {
         LevelAchievementPanel = FindObjectOfType<AchievementUI_Leveled>();
     }
 
+    private void Update()
+    {
+        if (needShowAds)
+        {
+            if (Advertisement.isInitialized && Advertisement.IsReady())
+            {
+                Advertisement.Show();
+                needShowAds = false;
+                needRestart = true;
+                needRestartTime = DateTime.Now;
+            }
+        }
+        if (needRestart && (DateTime.Now - needRestartTime).Seconds > 1)
+        {
+            gameObject.SetActive(false);
+
+            PlayerBehaviour player = GameObject.FindObjectOfType<PlayerBehaviour>();
+            player.enabled = true;
+            GameController.Instance.ResumeGame();
+            player.GrabAfterFall();
+        }
+    }
+
     public void RestartAfterFall()
     {
-        if (ads == null)
-            ads = StartCoroutine(StartAds());
+        needShowAds = true;
     }
 
     public void SetFailGame()
@@ -27,27 +53,5 @@ public class PanelAds : AchievementUI_Base
             LevelAchievementPanel.ShowBonusPanel(false);
         else
             GameController.Instance.FailGame();
-    }
-
-    IEnumerator StartAds()
-    {
-        while (!Advertisement.isInitialized || !Advertisement.IsReady())
-        {
-            yield return new WaitForSeconds(0.5f);
-        }
-        Advertisement.Show();
-        ads = null;
-
-        while (!Advertisement.isShowing)
-        {
-            yield return null;
-        }
-
-        gameObject.SetActive(false);
-
-        PlayerBehaviour player = GameObject.FindObjectOfType<PlayerBehaviour>();
-        player.enabled = true;
-        GameController.Instance.ResumeGame();
-        player.GrabAfterFall();
     }
 }
