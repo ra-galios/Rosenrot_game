d630ac09e7549d4035647c21b4a22bb0e5e10419
sonya-s-code <ksy.sonya.kot@gmail.com>
2017-07-15 23:14:10 +0300

    Исправила появление Button_tutorial

---
+++ Assets/Scripts/UI/Tutorial.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Tutorial : MonoBehaviour {
+	private void Start () {
+	    if (PlayerPrefs.GetString("Tutorial") != "NotFirstStart")
+	    {
+	        PlayerPrefs.SetString("Tutorial", "NotFirstStart");
+	    }
+	    else
+	    {
+	        HideTutorial();
+	    }
+    }
+
+    public void HideTutorial()
+    {
+        gameObject.GetComponent<Image>().enabled = false;
+        gameObject.transform.GetChild(0).gameObject.SetActive(false);
+    }
+
+    private void OnApplicationQuit()
+    {
+        PlayerPrefs.SetString("Tutorial", "FirstStart");
+    }
+}
